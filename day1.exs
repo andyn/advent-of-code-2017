2db87e5e086fdf3e7a06732cbe5d5d37b0f693aa
Antti Nilakari <antti.nilakari@corefactory.com>
2017-12-09 00:35:45 +0200

    Add comments for day 1

--- day1.exs
+++ day1.exs
@@ -1,31 +1,49 @@
 
 defmodule Day1 do
 
+    # Shorthand for zero accumulator
     def sum_matching_elements(list1, list2) do
         sum_matching_elements(list1, list2, 0)
     end
 
+    # If the head elements match, sum them
     def sum_matching_elements([head | tail1], [head | tail2], sum) do
          sum_matching_elements(tail1, tail2, sum + head)
     end
 
+    # If the heads differ, discard them
     def sum_matching_elements([_ | tail1], [_ | tail2], sum) do
         sum_matching_elements(tail1, tail2, sum)
     end
 
+    # Stop iteration on reaching the end of either list
     def sum_matching_elements([], _, sum), do: sum
     def sum_matching_elements(_, [], sum), do: sum
 
+    # Get the integer that a char code represents.
+    # No error handling.
     def digit_to_integer(char), do: char - ?0
+
 end
 
+
 defmodule Day1a do
+    """
+    Sum adjacent elements in a list if they are equal.
+    The last element is adjacent to the first one.
+    """
+
     def sum_adjacent_wrap(list = [head | tail]) do
         Day1.sum_matching_elements(list, tail ++ [head])
     end
 end
 
+
 defmodule Day1b do
+    """
+    Sum the elements modulo half of the list length, if they are equal.
+    """
+
     def sum_halfway_wrap(list) do
         half_length = length(list) |> div(2)
         [first_half, second_half] = Enum.chunk(list, half_length, half_length)
@@ -33,7 +51,10 @@ defmodule Day1b do
     end
 end
 
-
+# Read the input file.
+# Trim the whitespace.
+# Convert it to char values.
+# Conver the chars to integers.
 {:ok, handle} = File.open("day1.txt")
 input = IO.read(handle, :all)
   |> String.trim
