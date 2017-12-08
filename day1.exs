1dd05a91954f7df95af061cd26b1485639d33847
Antti Nilakari <antti.nilakari@corefactory.com>
2017-12-09 00:29:29 +0200

    Refactor common functionality to a common module

--- day1.exs
+++ day1.exs
@@ -1,22 +1,27 @@
 
+defmodule Day1 do
+
+    def sum_matching_elements(list1, list2) do
+        sum_matching_elements(list1, list2, 0)
+    end
+
+    def sum_matching_elements([head | tail1], [head | tail2], sum) do
+         sum_matching_elements(tail1, tail2, sum + head)
+    end
+
+    def sum_matching_elements([_ | tail1], [_ | tail2], sum) do
+        sum_matching_elements(tail1, tail2, sum)
+    end
+
+    def sum_matching_elements([], _, sum), do: sum
+    def sum_matching_elements(_, [], sum), do: sum
+
+    def digit_to_integer(char), do: char - ?0
+end
+
 defmodule Day1a do
-
-    def sum_adjacent_wrap(list = [head | _]) do
-        sum_adjacent(list ++ [head])
-    end
-
-    defp sum_adjacent(list), do: sum_adjacent(list, 0)
-
-    defp sum_adjacent([head, head | tail], sum) do
-        sum_adjacent([head | tail], sum + head - ?0)
-    end
-
-    defp sum_adjacent([_ | tail], sum) do
-        sum_adjacent(tail, sum)
-    end
-
-    defp sum_adjacent([], sum) do
-        sum
+    def sum_adjacent_wrap(list = [head | tail]) do
+        Day1.sum_matching_elements(list, tail ++ [head])
     end
 end
 
@@ -24,19 +29,8 @@ defmodule Day1b do
     def sum_halfway_wrap(list) do
         half_length = length(list) |> div(2)
         [first_half, second_half] = Enum.chunk(list, half_length, half_length)
-        sum_lists(list, second_half ++ first_half, 0)
+        Day1.sum_matching_elements(list, second_half ++ first_half, 0)
     end
-
-    defp sum_lists([head | tail1], [head | tail2], sum) do
-         sum_lists(tail1, tail2, sum + head - ?0)
-    end
-
-    defp sum_lists([_ | tail1], [_ | tail2], sum) do
-        sum_lists(tail1, tail2, sum)
-    end
-
-    defp sum_lists([], _, sum), do: sum
-    defp sum_lists(_, [], sum), do: sum
 end
 
 
@@ -44,6 +38,7 @@ end
 input = IO.read(handle, :all)
   |> String.trim
   |> to_charlist
+  |> Enum.map(&Day1.digit_to_integer/1)
 
 answer_a = Day1a.sum_adjacent_wrap input
 answer_b = Day1b.sum_halfway_wrap input
