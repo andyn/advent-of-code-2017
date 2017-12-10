7c1c7e25fb9feecf56e2f1eb62a9e27d4442f638
Antti Nilakari <antti.nilakari@iki.fi>
2017-12-11 00:24:10 +0200

    Add support for day 5 second star

--- day5.exs
+++ day5.exs
@@ -1,28 +1,35 @@
 
 defmodule Cpu do
 
-  def init(instructions) do
-    init instructions,
-         %{memory: %{}, memory_size: 0, pc: 0, cycles: 0}
+  # Default ALU for first star
+  def default_alu(x), do: x + 1
+
+  # ALU for second star
+  def complex_alu(x) when x >= 3, do: x - 1
+  def complex_alu(x), do: x + 1
+
+  def init(instructions, alu \\ &default_alu/1) do
+    init_cpu instructions,
+         %{memory: %{}, memory_size: 0, alu: alu, pc: 0, cycles: 0}
   end
 
-  def init([], cpu), do: cpu
+  def init_cpu([], cpu), do: cpu
 
-  def init([instruction | tail], cpu) do
+  def init_cpu([instruction | tail], cpu) do
     cpu = cpu
       |> put_in([:memory, cpu[:memory_size]], instruction)
       |> put_in([:memory_size], cpu[:memory_size] + 1)
-    init tail, cpu
+    init_cpu tail, cpu
   end
 
   # Out of bounds, return cpu in current state
   def run(cpu = %{pc: pc, memory_size: size}) when pc < 0 or pc >= size, do: cpu
 
   # Non-empty memory, run
-  def run(cpu = %{memory: mem, pc: pc, cycles: cycles}) do
+  def run(cpu = %{memory: mem, pc: pc, cycles: cycles, alu: alu}) do
     instruction = Map.get(mem, pc)
     # Execute
-    write_back = instruction + 1
+    write_back = alu.(instruction)
     # Update
     cpu = cpu
       |> put_in([:memory, pc], write_back)
@@ -48,7 +55,7 @@ count = File.stream!("day5-example.txt")
   |> Cpu.init
   |> Cpu.run
   |> Cpu.cycles_used
-IO.puts "Example: Cycles used after finishing: #{count}"
+IO.puts "First example: Cycles used after finishing: #{count}"
 
 count = File.stream!("day5-input.txt")
   |> Stream.map(&String.trim/1)
@@ -59,3 +66,23 @@ count = File.stream!("day5-input.txt")
   |> Cpu.run
   |> Cpu.cycles_used
 IO.puts "First star: Cycles used after finishing: #{count}"
+
+count = File.stream!("day5-example.txt")
+  |> Stream.map(&String.trim/1)
+  |> Stream.map(&Integer.parse/1)
+  |> Stream.map(fn {int, _} -> int end)
+  |> Enum.to_list
+  |> Cpu.init(&Cpu.complex_alu/1)
+  |> Cpu.run
+  |> Cpu.cycles_used
+IO.puts "Second example: Cycles used after finishing: #{count}"
+
+count = File.stream!("day5-input.txt")
+  |> Stream.map(&String.trim/1)
+  |> Stream.map(&Integer.parse/1)
+  |> Stream.map(fn {int, _} -> int end)
+  |> Enum.to_list
+  |> Cpu.init(&Cpu.complex_alu/1)
+  |> Cpu.run
+  |> Cpu.cycles_used
+IO.puts "Second star: Cycles used after finishing: #{count}"
