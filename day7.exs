588fe485c02521f35adb3c61e52b9dc4c0b144a9
Antti Nilakari <antti.nilakari@iki.fi>
2017-12-25 22:58:38 +0200

    Add day 7 second star, a rather ugly hack

--- day7.exs
+++ day7.exs
@@ -6,6 +6,7 @@ defmodule DiscTree do
     line_format = ~r/([a-z]+) \((\d+)\)( -> (.*))?/
     [_line, name, weight | maybe_children] = Regex.run(line_format, line)
     children = get_children(maybe_children)
+    {weight, _} = Integer.parse(weight)
     {name, weight, children}
   end
 
@@ -18,36 +19,89 @@ defmodule DiscTree do
       |> Enum.map(&String.trim/1)
   end
 
-  def build_tree(lines, nodes \\ %{}, orphans \\ %{})
+  def parse_tree(lines, nodes \\ %{}, orphans \\ %{})
 
-  def build_tree([], tree, orphans), do: remove_orphans tree, orphans
-
-  def build_tree([line | lines], nodes, orphans) do
-    {name, weight, children} = parse_line line
-    nodes = Map.put(nodes, name, {weight, children})
-    orphans = Map.put(orphans, name, :true)
-    build_tree lines, nodes, orphans
+  def parse_tree([], nodes, orphans) do
+    root_name = find_root nodes, orphans
+    into_tree root_name, nodes
   end
 
-  def remove_orphans(tree, orphans) do
-    all_children = Enum.flat_map(tree, fn {_, {_, orphans}} -> orphans end)
+  def parse_tree([line | lines], nodes, orphans) do
+    {name, weight, children} = parse_line line
+    nodes = Map.put(nodes, name, %{name: name, weight_of_self: weight, children: children})
+    orphans = Map.put(orphans, name, :true)
+    parse_tree lines, nodes, orphans
+  end
+
+  # Return the name of the root node
+  def find_root(nodes, orphans) do
+    all_children = Enum.flat_map(nodes, fn {_name, value} -> value.children end)
     [root] = orphans
       |> Map.drop(all_children)
       |> Map.keys
+    IO.puts "Root node is #{root}"
     root
   end
 
+  # Check if children have matching weight
+
+  def child_weights_match?([]), do: :true
+
+  def child_weights_match?([first | rest]) do
+    child_weights_match? rest, first
+  end
+
+  defp child_weights_match?([], _weight), do: :true
+
+  defp child_weights_match?([child | rest], weight) do
+    if child === weight do
+      child_weights_match? rest, weight
+    else
+      :false
+    end
+  end
+
+
+  def into_tree(name, nodes) do
+    self = nodes[name]
+    children = for child_name <- self.children do
+      into_tree(child_name, nodes)
+    end
+
+    weights_of_children = for child <- children, do: child.weight
+    weight_of_children = Enum.sum(weights_of_children)
+
+    if not child_weights_match? weights_of_children do
+      IO.puts "---"
+      IO.puts "Differing nodes, do the calculation yourself:"
+      for child <- children do
+        IO.puts "#{child.name}, #{child.weight_of_self}, #{child.weight}"
+      end
+      IO.puts "---"
+      throw "found"
+    end
+
+    self
+      |> Map.replace!(:children, children)
+      |> Map.put(:weight_of_children, weight_of_children)
+      |> Map.put(:weight, self.weight_of_self + weight_of_children)
+  end
 end
 
-"tknk" = "day7-example.txt"
-  |> File.stream!
-  |> Enum.to_list
-  |> DiscTree.build_tree
+try do
+  "day7-example.txt"
+    |> File.stream!
+    |> Enum.to_list
+    |> DiscTree.parse_tree
+catch
+  "found" -> :ok
+end
 
-
-tree_root = "day7-input.txt"
-  |> File.stream!
-  |> Enum.to_list
-  |> DiscTree.build_tree
-
-IO.puts "Tree root is #{tree_root}"
+try do
+  "day7-input.txt"
+    |> File.stream!
+    |> Enum.to_list
+    |> DiscTree.parse_tree
+catch
+  "found" -> :ok
+end
